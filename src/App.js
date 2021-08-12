2a94a502474f2878cb6dbe7688231006e2edddba
CARLOS <CMATURANA.QUESILLE@GMAIL.COM>
2021-08-12 18:20:54 -0400

    Pruebas react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+function Helloworld(props){
+    console.log(props);
+    return(
+        <div id="hello">
+            <h3>{props.mytext}</h3>
+            <p>{props.subtitulo}</p>
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            Este es mi componente:
+            <Helloworld mytext="Carlos!" subtitulo="Nuevo mensaje"/>
+            <Helloworld mytext="Miguell!" subtitulo="Nuevo manzanas"/>
+            <Helloworld mytext="Manuel!" subtitulo="Nuevo peras"/>
+        </div>
+    );
 }
 
 export default App;
